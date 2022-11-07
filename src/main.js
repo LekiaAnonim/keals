5b01e1aa060a1c616a929691da484e7c96e67ecd
Lekia Prosper <63610232+LekiaAnonim@users.noreply.github.com>
2022-11-07 16:08:52 +0100

    add video for vessel cleaning

--- src/main.js
+++ src/main.js
@@ -1,15 +1,3 @@
-$('body').toggleClass('loaded');
-$(document).ready(function() {
- 
-    setTimeout(function(){
-        $('body').addClass('loaded');
-        // $('h1').css('color','#222222');
-    }, 3000);
- 
-});
-
-
-
 
 window.addEventListener('scroll', function () {
   
